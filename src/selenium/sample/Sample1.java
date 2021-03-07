072595e254ddc2654276316cd636f239cabeb94c
avanish.patel <avanish.patel@accenture.com>
2021-03-07 23:03:07 +0200

    Chrome Driver Update to latest version

--- src/selenium/sample/Sample1.java
+++ src/selenium/sample/Sample1.java
@@ -6,31 +6,36 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.concurrent.TimeUnit;
+
 public class Sample1 {
     static String libWithDriversLocation = System.getProperty("user.dir") + "\\lib\\";
 
     @Test
     public void goToHomepage() throws Exception {
         //define driver
-        System.setProperty("webdriver.chrome.driver", libWithDriversLocation + "chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", libWithDriversLocation + "chromedriver89.exe");
         WebDriver driver = new ChromeDriver();
 
         //open test homepage
-        driver.get("https://google.com");
-      //  driver.get("https://kristinek.github.io/site/");
+//        driver.get("https://google.com");
+        driver.get("https://kristinek.github.io/site/");
 
-        driver.findElement(By.xpath("//input[@aria-label = `Search`]")).sendKeys("some text");
-        driver.findElement(By.xpath("//input[@type = `submit`]")).click();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
 
-//        System.out.println(driver.findElement(By.id("h1")).getText());
+        /*driver.findElement(By.xpath("//input[@aria-label = `Search`]")).sendKeys("some text");
+        driver.findElement(By.xpath("//input[@type = `submit`]")).click();*/
+
+        System.out.println(driver.findElement(By.id("h1")).getText());
 //        //get title of page
-//        System.out.println(driver.getTitle());
+        System.out.println(driver.getTitle());
 //
 //        //get URL of current page
-//        System.out.println(driver.getCurrentUrl());
+        System.out.println(driver.getCurrentUrl());
 //
 //        //Sleep for 10 seconds
-//        Thread.sleep(10000);
+        Thread.sleep(10000);
 
         //Close browser
         driver.quit();
