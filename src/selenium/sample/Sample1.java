ccb4979803f45272af71db14e9451763f5e7f6e3
Natalja Sniega <natalja.protasova@gmail.com>
2021-03-08 10:50:42 +0200

    Sample1 modification

--- src/selenium/sample/Sample1.java
+++ src/selenium/sample/Sample1.java
@@ -13,28 +13,28 @@ public class Sample1 {
 
     @Test
     public void goToHomepage() throws Exception {
-        //define driver
+        //define driver: chrome driver & folder location
         System.setProperty("webdriver.chrome.driver", libWithDriversLocation + "chromedriver89.exe");
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver = new ChromeDriver(); //create object of the Chrome driver
 
         //open test homepage
 //        driver.get("https://google.com");
-        driver.get("https://kristinek.github.io/site/");
+        driver.get("https://kristinek.github.io/site/tasks/enter_a_number"); //means which url I want to open
 
-        driver.manage().timeouts().implicitlyWait(25, TimeUnit.SECONDS);
-        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(25, TimeUnit.SECONDS); //to set waiting time to load the page <25 sec
+        driver.manage().window().maximize(); //will maximize the browser screen if I load the page
 
         /*driver.findElement(By.xpath("//input[@aria-label = `Search`]")).sendKeys("some text");
         driver.findElement(By.xpath("//input[@type = `submit`]")).click();*/
 
-        System.out.println(driver.findElement(By.id("h1")).getText());
+//        System.out.println(driver.findElement(By.id("h1")).getText());
 //        //get title of page
-        System.out.println(driver.getTitle());
+        System.out.println(driver.getTitle()); // to get the title on of opened site tab in browser
 //
 //        //get URL of current page
-        System.out.println(driver.getCurrentUrl());
+        System.out.println(driver.getCurrentUrl()); //returns current url of the site
 //
-//        //Sleep for 10 seconds
+//        //Sleep for 10 seconds : without this closes web-page immediately, this one sets time to show
         Thread.sleep(5000);
 
         //Close browser
