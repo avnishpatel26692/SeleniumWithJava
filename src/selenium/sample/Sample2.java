c27d1f0b253f6ed373f5c4151452466c2375b026
jana-martinsone <jana.martinsone.lv@gmail.com>
2021-03-08 12:58:05 +0200

    Sample2 add

---
+++ src/selenium/sample/Sample2.java
@@ -0,0 +1,40 @@
+package selenium.sample;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class Sample2 {
+    static String libWithDriversLocation = System.getProperty("user.dir") + "\\lib\\";
+
+    @Test
+    public void goToHomepage() throws Exception {
+        //define driver
+        System.setProperty("webdriver.chrome.driver", libWithDriversLocation + "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        //open test homepage
+        driver.get("https://kristinek.github.io/site/examples/locators");
+
+        driver.manage().timeouts().implicitlyWait(25, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+
+    //findElements
+        System.out.println(driver.findElement(By.id("heading_2")).getText());
+        System.out.println(driver.findElement(By.name("randomButton1")).getAttribute("value"));
+        System.out.println(driver.findElement(By.tagName("h2")).getAttribute("id"));
+        System.out.println(driver.findElement(By.className("text")).getText());
+
+
+
+//
+//        //Sleep for 10 seconds
+        Thread.sleep(3000);
+
+        //Close browser
+        driver.quit();
+    }
+}
