90024718ce5189917326ca140843e3f499b24454
nataljaserga <79837495+nataljaserga@users.noreply.github.com>
2021-03-08 10:51:57 +0200

    Sample1 modification

--- src/selenium/sample/Sample1.java
+++ src/selenium/sample/Sample1.java
@@ -9,17 +9,18 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.util.concurrent.TimeUnit;
 
 public class Sample1 {
-    static String libWithDriversLocation = System.getProperty("user.dir") + "\\lib\\";
+    static String libWithDriversLocation = System.getProperty("user.dir") + "/lib/";
+
 
     @Test
     public void goToHomepage() throws Exception {
         //define driver
-        System.setProperty("webdriver.chrome.driver", libWithDriversLocation + "chromedriver89.exe");
+        System.setProperty("webdriver.chrome.driver", libWithDriversLocation + "chromedriver");
         WebDriver driver = new ChromeDriver();
 
         //open test homepage
 //        driver.get("https://google.com");
-        driver.get("https://kristinek.github.io/site/");
+        driver.get("https://kristinek.github.io/site/tasks/enter_a_number");
 
         driver.manage().timeouts().implicitlyWait(25, TimeUnit.SECONDS);
         driver.manage().window().maximize();
