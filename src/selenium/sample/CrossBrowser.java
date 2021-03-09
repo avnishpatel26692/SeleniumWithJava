212895afa3be87f874054830d67e35581b216400
avanish.patel <avanish.patel@accenture.com>
2021-03-09 12:32:54 +0200

    Crossbrowser Added

--- src/selenium/sample/CrossBrowser.java
+++ src/selenium/sample/CrossBrowser.java
@@ -1,51 +1,42 @@
 package selenium.sample;
 
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
 import java.util.concurrent.TimeUnit;
 
-public class Sample7 {
+public class CrossBrowser {
 
     static String libWithDriversLocation = System.getProperty("user.dir") + "\\lib\\";
     WebDriver driver;
 
     @Before
     public void initBrowser() {
+        //Running in Chrome Headless
         System.setProperty("webdriver.chrome.driver", libWithDriversLocation + "chromedriver89.exe");
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+
+        //Run test in Firefox
+        /*System.setProperty("webdriver.gecko.driver",libWithDriversLocation + "geckodriver.exe");
+        driver = new FirefoxDriver();*/
+
+        //Run test in IE Browser
+        /*System.setProperty("webdriver.ie.driver",libWithDriversLocation + "IEDriverServer.exe");
+        driver = new InternetExplorerDriver();*/
 
         //open test homepage
         driver.get("https://kristinek.github.io/site/tasks/locators_different");
         driver.manage().timeouts().implicitlyWait(25, TimeUnit.SECONDS);
         driver.manage().window().maximize();
-    }
-
-    @Test
-    public void verifyFontsize(){
-        WebElement para4 = driver.findElement(By.cssSelector("p.w3-deep-purple"));
-        //font-size
-        System.out.println(para4.getCssValue("font-size"));
-        Assert.assertEquals("15px", para4.getCssValue("font-size"));
-
-        //font Family
-        System.out.println(para4.getCssValue("font-family"));
-        Assert.assertEquals("Verdana, sans-serif",para4.getCssValue("font-family"));
-
-        //background-color
-        System.out.println(para4.getCssValue("background-color"));
-        Assert.assertEquals("rgba(103, 58, 183, 1)",para4.getCssValue("background-color"));
-
-        //color
-        System.out.println(driver.findElement(By.cssSelector("p.w3-pink")).getCssValue("color"));
-        Assert.assertEquals("rgba(255, 255, 255, 1)", driver.findElement(By.cssSelector("p.w3-pink")).getCssValue("color"));
     }
 
     @Test
@@ -73,4 +64,5 @@ public class Sample7 {
         //Close browser
         driver.quit();
     }
+
 }
