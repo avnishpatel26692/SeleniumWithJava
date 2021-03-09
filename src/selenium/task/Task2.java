f7e60519815c6733f266d6109d3aaa0a263e346f
avanish.patel <avanish.patel@accenture.com>
2021-03-09 10:24:35 +0200

    Sample6 Added

---
+++ src/selenium/task/Task2.java
@@ -0,0 +1,88 @@
+package selenium.task;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class Task2 {
+
+    static String libWithDriversLocation = System.getProperty("user.dir") + "\\lib\\";
+    WebDriver driver;
+
+    @Before
+    public void initBrowser() {
+        System.setProperty("webdriver.chrome.driver", libWithDriversLocation + "chromedriver89.exe");
+        driver = new ChromeDriver();
+
+        //open test homepage
+        driver.get("https://kristinek.github.io/site/tasks/provide_feedback");
+        driver.manage().timeouts().implicitlyWait(25, TimeUnit.SECONDS);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        Thread.sleep(5000);
+
+        //Close browser
+        driver.quit();
+    }
+
+    @Test
+    public void initialFeedbackPage() throws Exception {
+//         TODO:
+//         check that all field are empty and no tick are clicked
+//         "Don't know" is selected in "Genre"
+//         "Choose your option" in "How do you like us?"
+//         check that the button send is blue with white letters
+    }
+
+    @Test
+    public void emptyFeedbackPage() throws Exception {
+//         TODO:
+//         click "Send" without entering any data
+//         check fields are empty or null
+//         check button colors
+//         (green with white letter and red with white letters)
+    }
+
+    @Test
+    public void notEmptyFeedbackPage() throws Exception {
+//         TODO:
+//         fill the whole form, click "Send"
+//         check fields are filled correctly
+//         check button colors
+//         (green with white letter and red with white letters)
+    }
+
+    @Test
+    public void yesOnWithNameFeedbackPage() throws Exception {
+//         TODO:
+//         enter only name
+//         click "Send"
+//         click "Yes"
+//         check message text: "Thank you, NAME, for your feedback!"
+//         color of text is white with green on the background
+    }
+
+    @Test
+    public void yesOnWithoutNameFeedbackPage() throws Exception {
+//         TODO:
+//         click "Send" (without entering anything
+//         click "Yes"
+//         check message text: "Thank you for your feedback!"
+//         color of text is white with green on the background
+    }
+
+    @Test
+    public void noOnFeedbackPage() throws Exception {
+//         TODO:
+//         fill the whole form
+//         click "Send"
+//         click "No"
+//         check fields are filled correctly
+    }
+}
