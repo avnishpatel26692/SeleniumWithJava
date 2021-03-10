78c983ca395cb32437cfd10939b8f865f662b035
Egils Lapins <egilslapins@gmail.com>
2021-03-10 10:25:38 +0200

    Sample 10 - loadingExample1

---
+++ src/selenium/sample/Sample10_Loading.java
@@ -0,0 +1,41 @@
+package selenium.sample;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Sample10_Loading {
+    static String libWithDriversLocation = System.getProperty("user.dir") + "\\lib\\";
+    WebDriver driver;
+
+    @Before
+    public void initBrowser() {
+        System.setProperty("webdriver.chrome.driver", libWithDriversLocation + "chromedriver88.exe");
+        driver = new ChromeDriver();
+        driver.get("https://kristinek.github.io/site/examples/loading_color");
+    }
+    @Test
+    public void loadingExample1()
+    {
+        driver.findElement(By.id("start_green")).click();
+        WebDriverWait wait = new WebDriverWait(driver, 15);
+        String check1;
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//h2[@id='finish_green']")));
+                 check1 = driver.findElement(By.xpath("//h2[@id='finish_green']")).getText();
+        Assert.assertEquals("Green Loaded", check1);
+    }
+
+
+    @After
+    public void tearDown() throws Exception {
+
+        //Close browser
+        driver.quit();
+    }
+}
